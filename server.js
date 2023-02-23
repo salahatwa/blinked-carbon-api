4bdc64cca55427842dcf09b9c8cd7ca3bd9b0724
salahatwa <salahsayedatwa@gmail.com>
2023-02-23 14:09:58 +0200

    adding repo

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var fs = require("fs");
 var mime = require('mime');
 var bodyParser = require('body-parser');
 const { Readable } = require('stream');
+const path = require( "path" );
+const log = console.log;
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +25,22 @@ app.post('/api/template/render', function (req, res) {
 
   try {
     //options
+
+    const folder = './';
+
+    fs.readdirSync(folder).forEach(file => {
+
+      const extname = path.extname(file);
+      const filename = path.basename(file, extname);
+      const absolutePath = path.resolve(folder, file);
+
+      log("File : ", file);
+      log("filename : ", filename);
+      log("extname : ", extname);
+      log("absolutePath : ", absolutePath);
+
+    });
+
     carbone.render(templatePath, data, function (err, result) {
       if (err) return console.log(err);
 
@@ -37,7 +55,7 @@ app.post('/api/template/render', function (req, res) {
 
       // fs.writeFileSync('./result.pdf', result);
       // fs.writeFileSync(fileName, result);
-      console.log("File has been Generated successfully", fileName);
+      log("File has been Generated successfully", fileName);
     });
   } catch (e) {
     const errorMsg = {
@@ -55,7 +73,7 @@ const PORT = process.env.PORT || 8081;
 var server = app.listen(PORT, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log("Example app listening at http://127.0.0.1:%s", host, port)
+  log("Example app listening at http://127.0.0.1:%s", host, port)
 })
 
 
